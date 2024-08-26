b7cd65636068b34f7a6261a85cda8b03c1ec597d
Sasha Pudkova <sashapudkova@bk.ru>
2024-08-26 20:14:45 +0300

    second commit but not finish

--- src/js/__tests__/team.tests.js
+++ src/js/__tests__/team.tests.js
@@ -1,11 +1,28 @@
 import Team from "../class/team";
 
+// test('work in team', () => { 
+//     const team = new Team();
+//     team.add('Sasha')
+//     expect(() => team.add('Sasha')).toThrow();
+//     expect(() => team.addAll('Masha', 'Petr')).not.toThrow();
+//     expect(team.members.has('Masha')).toBe('true');
+//     expect(team.toArray()).toBeInstanceOf(Array);
+// })
+
+test('work in team array', () => { 
+    const team = new Team();
+    team.add('Sasha')
+    expect(team.toArray()).toEqual(['Sasha']);
+});
+
 test('work in team', () => { 
     const team = new Team();
     team.add('Sasha')
-    expect(() => team.add('Sasha')).toThrow();
-    expect(() => team.addAll('Masha', 'Petr')).not.toThrow();
-    expect(team.members.has('Masha')).toBe('true');
-    expect(team.toArray()).toBeInstanceOf(Array);
-})
+    expect(() => team.add('Sasha')).toBe('Such an object already exists in the team')
+});
 
+test('work in team all', () => { 
+    const team = new Team();
+    team.add('Ivan', 'Petr')
+    expect(team.toArray()).toEqual(['Ivan', 'Petr'])
+})
